2f4fb61add5d6801fc18100cc67407edd2d6a8d8
MūmüìśDõg <wretchcca100900+1@gmail.com>
2025-05-09 16:48:00 +0800

    Update DYYYDownloadProgressView.m

--- DYYYDownloadProgressView.m
+++ DYYYDownloadProgressView.m
@@ -347,11 +347,12 @@
   CAShapeLayer *crossLayer = [CAShapeLayer layer];
   
   UIBezierPath *crossPath = [UIBezierPath bezierPath];
-  [crossPath moveToPoint:CGPointMake(circleSize * 0.3, circleSize * 0.3)];
-  [crossPath addLineToPoint:CGPointMake(circleSize * 0.7, circleSize * 0.7)];
+
   [crossPath moveToPoint:CGPointMake(circleSize * 0.7, circleSize * 0.3)];
   [crossPath addLineToPoint:CGPointMake(circleSize * 0.3, circleSize * 0.7)];
-
+  [crossPath moveToPoint:CGPointMake(circleSize * 0.3, circleSize * 0.3)];
+  [crossPath addLineToPoint:CGPointMake(circleSize * 0.7, circleSize * 0.7)];
+  
   crossLayer.path = crossPath.CGPath;
   crossLayer.fillColor = nil;
   crossLayer.strokeColor = [UIColor whiteColor].CGColor;
