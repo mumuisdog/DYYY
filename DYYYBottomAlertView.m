52e232aa10b3862f94e2cb6d94ae1da7a53f9ddb
MūmüìśDõg <wretchcca100900+1@gmail.com>
2025-06-11 21:33:51 +0800

    Update DYYYBottomAlertView.m

--- DYYYBottomAlertView.m
+++ DYYYBottomAlertView.m
@@ -1,11 +1,9 @@
 #import "DYYYBottomAlertView.h"
 #import "AwemeHeaders.h"
-
 #import "DYYYUtils.h"
 
 @implementation DYYYBottomAlertView
 
-// 自定义方法，支持同时自定义取消和确认按钮文本
 + (UIViewController *)showAlertWithTitle:(NSString *)title
                                  message:(NSString *)message
                          cancelButtonText:(NSString *)cancelButtonText
@@ -15,7 +13,6 @@
     
     AFDPrivacyHalfScreenViewController *vc = [NSClassFromString(@"AFDPrivacyHalfScreenViewController") new];
     
-    // 使用默认值处理空参数
     if (!cancelButtonText) {
         cancelButtonText = @"取消";
     }
@@ -24,31 +21,20 @@
         confirmButtonText = @"確定";
     }
 
-    DYYYAlertActionHandler wrappedCancelAction = nil;
-    if (cancelAction) {
-        wrappedCancelAction = ^{
-            [self dismissAlertViewController:vc];
+    DYYYAlertActionHandler wrappedCancelAction = ^{
+        [self dismissAlertViewController:vc];
+        if (cancelAction) {
             cancelAction();
-        };
-    } else {
-        wrappedCancelAction = ^{
-            [self dismissAlertViewController:vc];
-        };
-    }
-    
-    DYYYAlertActionHandler wrappedConfirmAction = nil;
-    if (confirmAction) {
-        wrappedConfirmAction = ^{
-            [self dismissAlertViewController:vc];
+        }
+    };
+
+    DYYYAlertActionHandler wrappedConfirmAction = ^{
+        [self dismissAlertViewController:vc];
+        if (confirmAction) {
             confirmAction();
-        };
-    } else {
-        wrappedConfirmAction = ^{
-            [self dismissAlertViewController:vc];
-        };
-    }
-    
-    // 设置滑动关闭和点击关闭的处理
+        }
+    };
+
     [vc setSlideDismissBlock:^{
         [self dismissAlertViewController:vc];
     }];
@@ -57,13 +43,6 @@
         [self dismissAlertViewController:vc];
     }];
     
-    // 设置关闭后的回调
-    [vc setAfterDismissBlock:^{
-        if (vc.view.superview) {
-            [self dismissAlertViewController:vc];
-        }
-    }];
-
     [vc configWithImageView:nil 
                   lockImage:nil 
            defaultLockState:NO 
@@ -74,45 +53,140 @@
        rightBtnClickedBlock:wrappedConfirmAction
       leftButtonClickedBlock:wrappedCancelAction];
     
-    // 设置圆角
-    [vc setCornerRadius:16.0];
-    [vc setOnlyTopCornerClips:YES];
-    
     [vc setUseCardUIStyle:YES];
 
-    // 使用 keyWindow 直接添加视图
-    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    [vc.view setFrame:window.bounds];
-    [window addSubview:vc.view];
-    
-    // 将视图控制器作为子视图控制器添加到根视图控制器
-    UIViewController *topVC = topView();
-    [topVC addChildViewController:vc];
-    [vc didMoveToParentViewController:topVC];
-    
+    UIViewController *topVC = topView(); 
+    if (topVC) {
+        if ([vc respondsToSelector:@selector(presentOnViewController:)]) {
+            [vc presentOnViewController:topVC];
+        }
+    }
     return vc;
 }
 
-// 添加用于移除弹窗的辅助方法
 + (void)dismissAlertViewController:(UIViewController *)viewController {
     if (!viewController) return;
     
-    [viewController willMoveToParentViewController:nil];
-    [viewController.view removeFromSuperview];
-    [viewController removeFromParentViewController];
+    if ([NSThread isMainThread]) {
+        [viewController dismissViewControllerAnimated:YES completion:nil];
+    }
 }
 
-// 原始方法保持不变，维持向后兼容性
 + (UIViewController *)showAlertWithTitle:(NSString *)title
                                  message:(NSString *)message
                             cancelAction:(DYYYAlertActionHandler)cancelAction
                            confirmAction:(DYYYAlertActionHandler)confirmAction {
-    return [self showAlertWithTitle:title 
-                            message:message 
-                    cancelButtonText:@"取消" 
-                   confirmButtonText:@"確定" 
-                        cancelAction:cancelAction 
+    return [self showAlertWithTitle:title
+                            message:message
+                    cancelButtonText:@"取消"
+                   confirmButtonText:@"確定"
+                        cancelAction:cancelAction
                        confirmAction:confirmAction];
+}
+
++ (UIViewController *)showAlertWithTitle:(NSString *)title
+                                 message:(NSString *)message
+                               avatarURL:(NSString *)avatarURL
+                            cancelAction:(DYYYAlertActionHandler)cancelAction
+                           confirmAction:(DYYYAlertActionHandler)confirmAction {
+    return [self showAlertWithTitle:title
+                            message:message
+                          avatarURL:avatarURL
+                    cancelButtonText:@"取消"
+                   confirmButtonText:@"確定"
+                        cancelAction:cancelAction
+                       confirmAction:confirmAction];
+}
+
++ (UIViewController *)showAlertWithTitle:(NSString *)title
+                                 message:(NSString *)message
+                               avatarURL:(NSString *)avatarURL
+                         cancelButtonText:(NSString *)cancelButtonText
+                        confirmButtonText:(NSString *)confirmButtonText
+                            cancelAction:(DYYYAlertActionHandler)cancelAction
+                           confirmAction:(DYYYAlertActionHandler)confirmAction {    
+
+    AFDPrivacyHalfScreenViewController *vc = [NSClassFromString(@"AFDPrivacyHalfScreenViewController") new];
+
+    if (!cancelButtonText) {
+        cancelButtonText = @"取消";
+    }
+    
+    if (!confirmButtonText) {
+        confirmButtonText = @"確定";
+    }
+
+    UIImageView *imageView = nil;
+    if (avatarURL.length > 0) {
+        imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 60, 60)];
+        imageView.translatesAutoresizingMaskIntoConstraints = NO;
+        [imageView.widthAnchor constraintEqualToConstant:60].active = YES;
+        [imageView.heightAnchor constraintEqualToConstant:60].active = YES;
+        imageView.layer.cornerRadius = 30;
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        imageView.layer.masksToBounds = YES;
+        imageView.clipsToBounds = YES;
+        
+        // 设置默认占位图
+        imageView.image = [UIImage imageNamed:@"AppIcon60x60"];
+        
+        // 异步加载网络图片
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:avatarURL]];
+            if (imageData) {
+                UIImage *image = [UIImage imageWithData:imageData];
+                if (image) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        imageView.image = image;
+                    });
+                }
+            }
+        });
+    }
+    
+    DYYYAlertActionHandler wrappedCancelAction = ^{
+        [self dismissAlertViewController:vc];
+        if (cancelAction) {
+            cancelAction();
+        }
+    };
+    
+    DYYYAlertActionHandler wrappedConfirmAction = ^{
+        [self dismissAlertViewController:vc];
+        if (confirmAction) {
+            confirmAction();
+        }
+    };
+    
+    [vc setSlideDismissBlock:^{
+        [self dismissAlertViewController:vc];
+    }];
+    
+    [vc setTapDismissBlock:^{
+        [self dismissAlertViewController:vc];
+    }];
+    
+    [vc configWithImageView:imageView
+                  lockImage:nil
+           defaultLockState:NO
+            titleLabelText:title
+          contentLabelText:message
+      leftCancelButtonText:cancelButtonText
+    rightConfirmButtonText:confirmButtonText
+       rightBtnClickedBlock:wrappedConfirmAction
+      leftButtonClickedBlock:wrappedCancelAction];
+    
+    [vc setCornerRadius:11];
+    [vc setOnlyTopCornerClips:YES];
+    
+    UIViewController *topVC = topView();
+    if (topVC) {
+        if ([vc respondsToSelector:@selector(presentOnViewController:)]) {
+            [vc presentOnViewController:topVC];
+        }
+    }
+    
+    return vc;
 }
 
 @end
