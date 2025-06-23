36a65dccb8e88f8b52a8dceabedd0988271b5859
MūmüìśDõg <wretchcca100900+1@gmail.com>
2025-06-23 22:25:13 +0800

    Update DYYYSettingsHelper.m

--- DYYYSettingsHelper.m
+++ DYYYSettingsHelper.m
@@ -1,9 +1,12 @@
 #import "DYYYSettingsHelper.h"
 #import "DYYYUtils.h"
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#import "DYYYImagePickerDelegate.h"
 
 #import "DYYYAboutDialogView.h"
 #import "DYYYCustomInputView.h"
+#import "DYYYIconOptionsDialogView.h"
 
 @implementation DYYYSettingsHelper
 
@@ -426,6 +429,194 @@
     }
 
     return item;
+}
+
+#pragma mark
+
+extern void showDYYYSettingsVC(UIViewController *rootVC, BOOL hasAgreed);
+extern void *kViewModelKey;
+
+@class DYYYImagePickerDelegate;
+
+static void showIconOptionsDialog(NSString *title, UIImage *previewImage, NSString *saveFilename, void (^onClear)(void), void (^onSelect)(void)) {
+    DYYYIconOptionsDialogView *optionsDialog = [[DYYYIconOptionsDialogView alloc] initWithTitle:title previewImage:previewImage];
+    optionsDialog.onClear = onClear;
+    optionsDialog.onSelect = onSelect;
+    [optionsDialog show];
+}
+
++ (AWESettingItemModel *)createIconCustomizationItemWithIdentifier:(NSString *)identifier
+                                  title:(NSString *)title
+                               svgIcon:(NSString *)svgIconName
+                            saveFile:(NSString *)saveFilename {
+    AWESettingItemModel *item = [[NSClassFromString(@"AWESettingItemModel") alloc] init];
+    item.identifier = identifier;
+    item.title = title;
+
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *dyyyFolderPath = [documentsPath stringByAppendingPathComponent:@"DYYY"];
+    NSString *imagePath = [dyyyFolderPath stringByAppendingPathComponent:saveFilename];
+
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:imagePath];
+    item.detail = fileExists ? @"已設定" : @"預設";
+
+    item.type = 0;
+    item.svgIconImageName = svgIconName;
+    item.cellType = 26;
+    item.colorStyle = 0;
+    item.isEnable = YES;
+
+    __weak AWESettingItemModel *weakItem = item;
+    item.cellTappedBlock = ^{
+        if (![[NSFileManager defaultManager] fileExistsAtPath:dyyyFolderPath]) {
+            [[NSFileManager defaultManager] createDirectoryAtPath:dyyyFolderPath withIntermediateDirectories:YES attributes:nil error:nil];
+        }
+
+        UIViewController *topVC = topView();
+
+        UIImage *previewImage = nil;
+        if (fileExists) {
+            previewImage = [UIImage imageWithContentsOfFile:imagePath];
+        }
+
+        showIconOptionsDialog(title, previewImage, saveFilename, ^{
+          if ([[NSFileManager defaultManager] fileExistsAtPath:imagePath]) {
+              NSError *error = nil;
+              [[NSFileManager defaultManager] removeItemAtPath:imagePath error:&error];
+              if (!error) {
+                  weakItem.detail = @"預設";
+                  [weakItem refreshCell];
+              }
+          }
+        }, ^{
+          UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+          picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+          picker.allowsEditing = NO;
+          picker.mediaTypes = @[ @"public.image" ];
+
+          DYYYImagePickerDelegate *pickerDelegate = [[DYYYImagePickerDelegate alloc] init];
+          pickerDelegate.completionBlock = ^(NSDictionary *info) {
+            NSURL *originalImageURL = info[UIImagePickerControllerImageURL];
+            if (!originalImageURL) {
+                originalImageURL = info[UIImagePickerControllerReferenceURL];
+            }
+            if (originalImageURL) {
+                NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+                NSString *dyyyFolderPath = [documentsPath stringByAppendingPathComponent:@"DYYY"];
+                NSString *imagePath = [dyyyFolderPath stringByAppendingPathComponent:saveFilename];
+
+                NSData *imageData = [NSData dataWithContentsOfURL:originalImageURL];
+                const char *bytes = (const char *)imageData.bytes;
+                BOOL isGIF = (imageData.length >= 6 && (memcmp(bytes, "GIF87a", 6) == 0 || memcmp(bytes, "GIF89a", 6) == 0));
+                if (isGIF) {
+                    [imageData writeToFile:imagePath atomically:YES];
+                } else {
+                    UIImage *selectedImage = [UIImage imageWithData:imageData];
+                    imageData = UIImagePNGRepresentation(selectedImage);
+                    [imageData writeToFile:imagePath atomically:YES];
+                }
+
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    weakItem.detail = @"已設定";
+                    [weakItem refreshCell];
+                });
+            }
+          };
+
+          static char kDYYYPickerDelegateKey;
+          picker.delegate = pickerDelegate;
+          objc_setAssociatedObject(picker, &kDYYYPickerDelegateKey, pickerDelegate, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+          [topVC presentViewController:picker animated:YES completion:nil];
+        });
+    };
+
+    return item;
+}
+
++ (AWESettingSectionModel *)createSectionWithTitle:(NSString *)title items:(NSArray *)items {
+    return [self createSectionWithTitle:title footerTitle:nil items:items];
+}
+
++ (AWESettingSectionModel *)createSectionWithTitle:(NSString *)title footerTitle:(NSString *)footerTitle items:(NSArray *)items {
+    AWESettingSectionModel *section = [[NSClassFromString(@"AWESettingSectionModel") alloc] init];
+    section.sectionHeaderTitle = title;
+    section.sectionHeaderHeight = 40;
+    section.sectionFooterTitle = footerTitle;
+    section.useNewFooterLayout = YES;
+    section.type = 0;
+    section.itemArray = items;
+    return section;
+}
+
++ (AWESettingBaseViewController *)createSubSettingsViewController:(NSString *)title
+                                                        sections:(NSArray *)sectionsArray {
+    AWESettingBaseViewController *settingsVC = [[NSClassFromString(@"AWESettingBaseViewController") alloc] init];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if ([settingsVC.view isKindOfClass:[UIView class]]) {
+            Class navBarClass = NSClassFromString(@"AWENavigationBar");		
+            for (UIView *subview in settingsVC.view.subviews) {
+                if (navBarClass && [subview isKindOfClass:navBarClass]) {
+                    id navigationBar = subview;
+                    if ([navigationBar respondsToSelector:@selector(titleLabel)]) {
+                        UILabel *label = [navigationBar valueForKey:@"titleLabel"];
+                        label.text = title;
+                    }
+                    break;
+                }
+            }
+        }
+    });
+
+    AWESettingsViewModel *viewModel = [[NSClassFromString(@"AWESettingsViewModel") alloc] init];
+    viewModel.colorStyle = 0;
+    viewModel.sectionDataArray = sectionsArray;
+    objc_setAssociatedObject(settingsVC, &kViewModelKey, viewModel, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+    return settingsVC;
+}
+
++ (UIViewController *)findViewController:(UIResponder *)responder {
+    while (responder) {
+        if ([responder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController *)responder;
+        }
+        responder = [responder nextResponder];
+    }
+    return nil;
+}
+
++ (void)openSettingsWithViewController:(UIViewController *)vc {
+    BOOL hasAgreed = [[NSUserDefaults standardUserDefaults] boolForKey:@"DYYYUserAgreementAccepted"];
+    showDYYYSettingsVC(vc, hasAgreed);
+}
+
++ (void)openSettingsFromView:(UIView *)view {
+    UIViewController *currentVC = [self findViewController:view];
+    if ([currentVC isKindOfClass:NSClassFromString(@"AWELeftSideBarViewController")]) {
+        [self openSettingsWithViewController:currentVC];
+    }
+}
+
++ (void)addTapGestureToView:(UIView *)view target:(id)target {
+    view.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:target action:@selector(openDYYYSettings)];
+    [view addGestureRecognizer:tapGesture];
+}
+
++ (void)showUserAgreementAlert {
+    [self showTextInputAlert:@"用戶協議" defaultText:@"" placeholder:@"" onConfirm:^(NSString *text) {
+        if ([text isEqualToString:@"我已閱讀並同意繼續使用"]) {
+            [self setUserDefaults:@"YES" forKey:@"DYYYUserAgreementAccepted"];
+        } else {
+            [DYYYUtils showToast:@"請正確輸入內容"];
+            [self showUserAgreementAlert];
+        }
+    } onCancel:^{
+        [DYYYUtils showToast:@"請立即移除本插件"];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            exit(0);
+        });
+    }];
 }
 
 @end
